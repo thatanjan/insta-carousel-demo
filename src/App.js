15aac5e9bb5175fa0e6a9c6507646e3b6dee2864
thatanjan <anjan.cules@gmail.com>
2021-08-01 17:00:52 +0600

    application is complete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,48 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
+
+import PostCard from './PostCard'
+
+const useStyles = makeStyles({
+	fullContainer: {
+		height: '100vh',
+	},
+	headerContainer: {
+		height: '25vh',
+		display: 'grid',
+		placeItems: 'center',
+	},
+
+	carouselOuterContainer: {
+		height: '75vh',
+	},
+})
+
 function App() {
-	return <div className='App'>Instagram</div>
+	const { fullContainer, headerContainer, carouselOuterContainer } = useStyles()
+	return (
+		<div className='App'>
+			<Grid container className={fullContainer}>
+				<Grid item className={headerContainer} xs={12}>
+					<Typography variant='h3'>Insta Carousel</Typography>
+				</Grid>
+
+				<Grid
+					item
+					container
+					justifyContent='center'
+					className={carouselOuterContainer}
+					xs={12}
+				>
+					<Grid item xs={3}>
+						<PostCard />
+					</Grid>
+				</Grid>
+			</Grid>
+		</div>
+	)
 }
 
 export default App
